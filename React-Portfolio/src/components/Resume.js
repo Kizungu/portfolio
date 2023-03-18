10b08579aa0af39feb8c19584be84a15d4ddd6d3
Kizungu <arnodserapiet@gmail.com>
2023-03-18 13:21:02 -0500

    ghfgh

--- React-Portfolio/src/components/Resume.js
+++ React-Portfolio/src/components/Resume.js
@@ -12,8 +12,7 @@ export default  class Resume extends Component {
             </div>
 
             <div className="nine columns main-col">
-              {
-                resumeData.education && resumeData.education.map((item)=>{
+              {resumeData.education && resumeData.education.map((item)=>{
                   return(
                     <div className="row item">
                        <div className="twelve columns">
